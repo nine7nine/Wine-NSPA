d1f81b67a4a14204ff956ac357d938b41fa54912
jordan Johnston <johnstonljordan@gmail.com>
2023-06-10 12:44:26 -0500

    Wine-NSPA-devel: build system: Update/Sync to wine-tkg-git
     -> Update/Sync to Wine-TKG-git (all patches, scripts, etc)
     -> userpatches: Fsync-Proton-current_pid-n-waiters.mypatch
    
     note: I sitll need to change the staging commit and rebase my build
     && patchwork. just doing this first, as a separate commit.
    
    Signed-off-by: jordan Johnston <johnstonljordan@gmail.com>

--- wine-tkg-git_devel/wine-tkg-scripts/build-32.sh
+++ wine-tkg-git_devel/wine-tkg-scripts/build-32.sh
@@ -6,11 +6,17 @@ _exports_32() {
 	fi
 	if [ -e /usr/bin/ccache ] && [ "$_NOMINGW" != "true" ]; then
 		export CROSSCC="ccache i686-w64-mingw32-gcc" && echo "CROSSCC32 = ${CROSSCC}" >>"$_LAST_BUILD_CONFIG"
+		export i386_CC="${CROSSCC}"
 	fi
   fi
   # build wine 32-bit
   if [ -d '/usr/lib32/pkgconfig' ]; then # Typical Arch path
     export PKG_CONFIG_PATH='/usr/lib32/pkgconfig'
+    # glib/gstreamer detection workaround for proton 8.0 trees
+    if [[ "$_plain_version" = *_8.0 ]]; then
+      CFLAGS+=" -I/usr/lib32/glib-2.0/include -I/usr/include/glib-2.0 -I/usr/include/gstreamer-1.0 -I/usr/lib32/gstreamer-1.0/include"
+      CROSSCFLAGS+=" -I/usr/lib32/glib-2.0/include -I/usr/include/glib-2.0 -I/usr/include/gstreamer-1.0 -I/usr/lib32/gstreamer-1.0/include"
+    fi
   elif [ -d '/usr/lib/i386-linux-gnu/pkgconfig' ]; then # Ubuntu 18.04/19.04 path
     export PKG_CONFIG_PATH='/usr/lib/i386-linux-gnu/pkgconfig'
   else
@@ -18,6 +24,10 @@ _exports_32() {
     # Workaround for Fedora freetype2 libs not being detected now that it's been moved to a subdir
     CFLAGS+=" -I/usr/include/freetype2"
     CROSSCFLAGS+=" -I/usr/include/freetype2"
+    if [[ "$_plain_version" = *_8.0 ]]; then
+      CFLAGS+=" -I/usr/lib/glib-2.0/include -I/usr/include/glib-2.0 -I/usr/include/gstreamer-1.0 -I/usr/lib/gstreamer-1.0/include"
+      CROSSCFLAGS+=" -I/usr/lib/glib-2.0/include -I/usr/include/glib-2.0 -I/usr/include/gstreamer-1.0 -I/usr/lib/gstreamer-1.0/include"
+    fi
   fi
 }
 
